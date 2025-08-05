330954dec0993aaf3e82bac15048a888156b4c11
Derrick Agyekum <agyekumderrickjnr@gmail.com>
2025-08-05 00:30:06 -0400

    Initial Commit

---
+++ Kdad-Library-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kdad-Library-App
+//
+//  Created by user on 8/5/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Kdad-Library-AppTests/Kdad_Library_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Kdad_Library_AppTests.swift
+//  Kdad-Library-AppTests
+//
+//  Created by user on 8/5/25.
+//
+
+import Testing
+@testable import Kdad_Library_App
+
+struct Kdad_Library_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
